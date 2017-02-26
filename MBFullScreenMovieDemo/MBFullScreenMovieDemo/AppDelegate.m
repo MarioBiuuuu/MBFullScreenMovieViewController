15a6722a25cfaad611e2500d3114aacd3ed32ff2
Yuri-Zhang <zhangxiaofei@zhangxiaofeideMacBook-Pro.local>
2017-02-26 14:35:42 +0800

    fix some bug

--- MBFullScreenMovieDemo/MBFullScreenMovieDemo/AppDelegate.m
+++ MBFullScreenMovieDemo/MBFullScreenMovieDemo/AppDelegate.m
@@ -21,38 +21,35 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    self.mb = [[MBFullScreenLaunghViewController alloc] initWithLaunghStyle:MBFullScreenLaunghStyleImage];
-//    self.mb = [[MBFullScreenLaunghViewController alloc] initWithLaunghStyle:MBFullScreenLaunghStyleGif];
-    
-    MBImageObject *obj = [[MBImageObject alloc] init];
-//    obj.localImage = [UIImage imageNamed:@"1111.jpeg"];
-//    obj.localImagePath = [[NSBundle mainBundle] pathForResource:@"1111.jpeg" ofType:nil];
-    obj.imageUrl = [NSURL URLWithString:@"http://imgsrc.baidu.com/forum/w%3D580/sign=e285eb946259252da3171d0c049a032c/3d998801a18b87d61c03a291050828381e30fd04.jpg"];
-//    obj.gifUrl = [NSURL URLWithString:@"http://img2.imgtn.bdimg.com/it/u=3366350805,2146995399&fm=23&gp=0.jpg"];
-    
-//    self.mb.imageArray = @[obj];
-    self.mb.imageObject = obj;
-    
-    self.mb.countDownTime = 10;
-    
-    UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    
-    self.mb.appRootViewController = [mainStoryBoard instantiateViewControllerWithIdentifier:@"MainViewController"];
+//    self.mb = [[MBFullScreenLaunghViewController alloc] initWithLaunghStyle:MBFullScreenLaunghStyleImage];
+////    self.mb = [[MBFullScreenLaunghViewController alloc] initWithLaunghStyle:MBFullScreenLaunghStyleGif];
+//    
+//    MBImageObject *obj = [[MBImageObject alloc] init];
+////    obj.localImage = [UIImage imageNamed:@"1111.jpeg"];
+////    obj.localImagePath = [[NSBundle mainBundle] pathForResource:@"1111.jpeg" ofType:nil];
+//    obj.imageUrl = [NSURL URLWithString:@"http://imgsrc.baidu.com/forum/w%3D580/sign=e285eb946259252da3171d0c049a032c/3d998801a18b87d61c03a291050828381e30fd04.jpg"];
+////    obj.gifUrl = [NSURL URLWithString:@"http://img2.imgtn.bdimg.com/it/u=3366350805,2146995399&fm=23&gp=0.jpg"];
+//    
+////    self.mb.imageArray = @[obj];
+//    self.mb.imageObject = obj;
+//    
+//    self.mb.countDownTime = 10;
+//    
     
 #warning 打开以下代码 看视频， （注释上面部分）
-//    self.mb = [[MBFullScreenLaunghViewController alloc] initWithLaunghStyle:MBFullScreenLaunghStyleMovie];
-//    self.mb.delegate = self;
-//    
-//    [self.mb mb_moviePlayComplate:^(MBMoviePlayerViewController *playerViewController, NSTimeInterval totalInterval) {
-//        NSLog(@"block ---> complaet : %@", @(totalInterval));
-//    }];
-//    
-//    [self.mb mb_moviePlayEnterBtnClick:^(UIButton *enterBtn) {
-//        NSLog(@"block ---> enter : %@", enterBtn);
-//    }];
-//    self.mb.loopCount = 2;
-//
-//    self.mb.videoUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"video.mp4"ofType:nil]];
+    self.mb = [[MBFullScreenLaunghViewController alloc] initWithLaunghStyle:MBFullScreenLaunghStyleMovie];
+    self.mb.delegate = self;
+    
+    [self.mb mb_moviePlayComplate:^(MBMoviePlayerViewController *playerViewController, NSTimeInterval totalInterval) {
+        NSLog(@"block ---> complaet : %@", @(totalInterval));
+    }];
+    
+    [self.mb mb_moviePlayEnterBtnClick:^(UIButton *enterBtn) {
+        NSLog(@"block ---> enter : %@", enterBtn);
+    }];
+    self.mb.loopCount = 2;
+
+    self.mb.videoUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"video.mp4"ofType:nil]];
     
     self.window.rootViewController = self.mb;
 
@@ -60,6 +57,11 @@
 //    UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
 //    
 //    mb.frontViewController = [mainStoryBoard instantiateViewControllerWithIdentifier:@"MainViewController"];
+    
+    UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+
+    self.mb.appRootViewController = [mainStoryBoard instantiateViewControllerWithIdentifier:@"MainViewController"];
+
     return YES;
 }
 
