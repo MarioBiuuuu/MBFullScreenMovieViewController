563aace02344e9a49d7ad0ee41d010ae585378b1
Yuri <ZhangXiaofei@Yuri.local>
2017-02-27 10:41:46 +0800

    modify product

--- MBFullScreenMovieDemo/MBFullScreenMovieDemo/ViewController.m
+++ MBFullScreenMovieDemo/MBFullScreenMovieDemo/ViewController.m
@@ -16,14 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
